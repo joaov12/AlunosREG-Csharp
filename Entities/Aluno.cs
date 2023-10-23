3b480905069da6936410cc683aa7f9af1ee5da45
Joao Vitor <jvfs12th@gmail.com>
2023-10-22 21:00:29 -0300

    First Commit: entitie Aluno

---
+++ Entities/Aluno.cs
@@ -0,0 +1,46 @@
+﻿namespace AlunosCRUD.Entities
+{
+    class Aluno
+    {
+        Random numAleatorio = new Random();
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string  Curso { get; set; }
+        public int Idade { get; set; }
+        public string  Email { get; set; }
+        public int Matricula { get; private set; }
+
+        public Aluno()
+        {
+            
+        }
+
+        public Aluno(int id, string nome, string curso, int idade, string email, int matricula)
+        {
+            Id = id;
+            Nome = nome;
+            Curso = curso;
+            Idade = idade;
+            Email = email;
+            Matricula = matricula;
+        }
+
+        public int SetId()
+        {
+            Id = numAleatorio.Next(1, 100);
+            return Id;
+        }
+
+        public int SetMatricula()
+        {
+            Matricula = numAleatorio.Next(1000, 9999);
+            return Matricula;
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + Id + ", Nome: " + Nome + ", Curso: " + Curso + ", Idade: " + Idade + ", Email: " + Email + ", Matricula: " +Matricula;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using AlunosCRUD.Entities;
+
+namespace AlunosCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Aluno> listaDeAluno = new List<Aluno>();
+            Aluno aluno = new Aluno();
+            
+
+            Console.WriteLine("Bem-vindo! Aqui você irá conseguir fazer o registro de todos os seus alunos");
+
+            Console.Write("Você vai querer adicionar quantos alunos agora? ");
+            int qtdAlunos = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtdAlunos; i++)
+            {
+                Console.WriteLine("OK! Vamos para o aluno #" + (i+1));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Curso: ");
+                string curso = Console.ReadLine();
+                Console.Write("Idade: ");
+                int idade = int.Parse(Console.ReadLine());
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                int id = aluno.SetId();
+                int matricula = aluno.SetMatricula();
+
+                listaDeAluno.Add(new Aluno(id ,nome, curso, idade, email, matricula));
+            }
+            foreach(Aluno aluno1 in listaDeAluno) 
+            { 
+                Console.WriteLine(aluno1.ToString());
+            }
+        }
+    }
+}
